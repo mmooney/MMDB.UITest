3714e5a6cdfd411dc18af7d66550f9c8aafee0d2
Mike Mooney <mmooney@mmdbsolutions.com>
2012-05-04 06:37:03 -0400

    working on target class manager tests

---
+++ MMDB.UITest.Generator.Library/TargetClassManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MMDB.UITest.DotNetParser;
+using MMDB.UITest.Core;
+
+namespace MMDB.UITest.Generator.Library
+{
+	public class TargetClassManager
+	{
+		public TargetClass TryLoadTargetClass(CSClass csClass)
+		{
+			TargetClass returnValue = null;
+			var uiClientPageAttribute = csClass.AttributeList.SingleOrDefault(i => i.TypeName == typeof(UIClientPageAttribute).Name && i.TypeNamespace == typeof(UIClientPageAttribute).Namespace);
+			if (uiClientPageAttribute != null)
+			{
+				returnValue = new TargetClass
+				{
+					SourceClassFullName = Convert.ToString(uiClientPageAttribute.GetAttributeParameter(0, "SourceClassFullName", true)),
+					TargetClassFullName = csClass.ClassFullName
+				};
+
+				//If there is only one field, that is the user and designer file.
+				//If there are two or more files and one ends with ".designer.cs", that is the designer file and the the first of the others is the user file
+				//If there are two or more files and none ends with ".designer.cs", then the first one is the designer and user file
+				if (csClass.FilePathList.Count == 1)
+				{
+					returnValue.DesignerFilePath = csClass.FilePathList[0];
+					returnValue.UserFilePath = csClass.FilePathList[0];
+				}
+				else if (csClass.FilePathList.Count > 1)
+				{
+					returnValue.DesignerFilePath = csClass.FilePathList.FirstOrDefault(i => i.EndsWith(".designer.cs", StringComparison.CurrentCultureIgnoreCase));
+					if (string.IsNullOrEmpty(returnValue.DesignerFilePath))
+					{
+						returnValue.DesignerFilePath = csClass.FilePathList[0];
+						returnValue.UserFilePath = csClass.FilePathList[0];
+					}
+					else
+					{
+						returnValue.UserFilePath = csClass.FilePathList.FirstOrDefault(i => i != returnValue.DesignerFilePath);
+					}
+				}
+
+				foreach (var csProperty in csClass.PropertyList)
+				{
+					var targetField = TargetField.TryLoad(csProperty);
+					if (targetField != null)
+					{
+						returnValue.TargetFieldList.Add(targetField);
+					}
+				}
+			}
+			return returnValue;
+		}
+	}
+}
---
+++ MMDB.UITest.Generator.Tests/TargetClassManagerTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using MMDB.UITest.DotNetParser;
+using MMDB.UITest.Generator.Library;
+
+namespace MMDB.UITest.Generator.Tests
+{
+	public class TargetClassManagerTests
+	{
+		public class TryLoadTargetClass
+		{
+			[Test]
+			public void BasicTest()
+			{
+				CSClass csClass = new CSClass()
+				{
+					ClassFullName = "Test.Target.TargetClassName",
+					AttributeList = new List<CSAttribute>() 
+					{
+						new CSAttribute
+						{
+							TypeFullName = typeof(UIClientPageAttribute).FullName,
+							ArgumentList = new List<CSAttribute.CSAttributeArgument>()
+							{
+								new CSAttribute.CSAttributeArgument { ArgumentName = "SourceClassFullName", ArguementValue = "Test.Test1.SourceClassFullNameValue" }
+							}
+						}
+					}
+				};
+				var targetClassManager = new TargetClassManager();
+				var targetClass = targetClassManager.TryLoadTargetClass(csClass);
+				Assert.IsNotNull(targetClass);
+				Assert.AreEqual("Test.Test1.SourceClassFullNameValue", targetClass.SourceClassFullName);
+				Assert.AreEqual("Test.Target.TargetClassName", targetClass.TargetClassFullName);
+				Assert.AreNotEqual(0,targetClass.TargetFieldList.Count);
+				Assert.AreNotEqual(EnumTargetObjectType.Unknown, targetClass.TargetObjectType);
+				Assert.IsNotNullOrEmpty(targetClass.PageUrl);
+			}
+		}
+	}
+}
