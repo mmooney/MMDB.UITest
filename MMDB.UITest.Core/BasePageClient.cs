81edc1ff004dde84f0c1a9e21e7fc6e71bf973df
Mike Mooney <mmooney@mmdbsolutions.com>
2012-07-14 06:39:29 -0400

    MMDB.UITest.Core v2.0.0.3
    Removed NUnit dependency
    Updated ExpectedUrl property to ExpectedUrlList

--- MMDB.UITest.Core/BasePageClient.cs
+++ MMDB.UITest.Core/BasePageClient.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using WatiN.Core;
-using NUnit.Framework;
 
 namespace MMDB.UITest.Core
 {
@@ -16,10 +15,31 @@ namespace MMDB.UITest.Core
 
 		public virtual void ValidateUrl()
 		{
-			string absolutePath = this.Browser.Uri.AbsolutePath;
-			Assert.AreEqual(this.ExpectedUrl.ToLower(), absolutePath.ToLower());
+			if(this.ExpectedUrlList.Count() > 0)
+			{
+				string absolutePath = this.Browser.Uri.AbsolutePath;
+				if(!this.ExpectedUrlList.Contains(absolutePath, StringComparer.CurrentCultureIgnoreCase))
+				{
+					StringBuilder sb = new StringBuilder();
+					sb.Append("Incorrect URL");
+					sb.Append("\tFound: " + absolutePath);
+					if(this.ExpectedUrlList.Count() == 1)
+					{
+						sb.Append("\t:Expected: " + this.ExpectedUrlList.Single());
+					}
+					else 
+					{
+						sb.Append("\tExpected:");
+						foreach(string expectedUrl in this.ExpectedUrlList)
+						{
+							sb.Append("\t-" + expectedUrl);
+						}
+					}
+					throw new Exception(sb.ToString());
+				}
+			}
 		}
 
-		protected abstract string ExpectedUrl { get; }
+		protected abstract IEnumerable<string> ExpectedUrlList { get; }
 	}
 }
